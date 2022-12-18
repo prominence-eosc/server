a04c600631ac3c02dd47464cffde6cf0a35d02fe
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-12-18 08:53:33 +0000

    Rename e to err

--- bin/worker_monitor.py
+++ bin/worker_monitor.py
@@ -15,8 +15,8 @@ from prominence.utilities import config, set_logger
 logger = set_logger(config().get('worker_monitor', 'log'))
 
 async def run():
-    async def error_cb(e):
-        logger.error(e)
+    async def error_cb(err):
+        logger.error(err)
 
     async def reconnected_cb():
         logger.error('Got reconnected to NATS')
