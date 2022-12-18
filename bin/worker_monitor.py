30d8119ba540688b167b2709a6a56c11e65c6480
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-12-18 08:10:05 +0000

    Log NATS connection errors, reconnections and closed connections

--- bin/worker_monitor.py
+++ bin/worker_monitor.py
@@ -15,6 +15,15 @@ from prominence.utilities import config, set_logger
 logger = set_logger(config().get('worker_monitor', 'log'))
 
 async def run():
+    async def error_cb(e):
+        logger.error(e)
+
+    async def reconnected_cb():
+        logger.error('Got reconnected to NATS')
+
+    async def closed_cb():
+        logger.error('Stopped reconnection to NATS')
+
     async def subscribe_handler(msg):
         data = msg.data.decode()
         data = json.loads(data)
@@ -24,7 +33,10 @@ async def run():
 
     nc = None
     try:
-        nc = await nats.connect(config().get('nats', 'url'))
+        nc = await nats.connect(config().get('nats', 'url'),
+                                reconnected_cb=reconnected_cb,
+                                closed_cb=closed_cb,
+                                error_cb=error_cb)
     except Exception as e:
         logger.error('Got exception connecting to NATS: %s', str(e))
         sys.exit(1)
